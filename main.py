c7e8414b1b10e17e8d6977c91dab760c6a2e5f47
Mateusz Krajewski <matikrajek42@gmail.com>
2024-06-09 15:28:54 +0200

    add OBJ + generate sphere

--- main.py
+++ main.py
@@ -4,8 +4,62 @@ from ursina import * # pylint: disable=all
 from gcodeparser import GCodeParser, Move # pylint: disable=all
 
 ###
-SLOWER = 100
+SLOWER = 10
 ###
+
+
+class Generator():
+    def __init__(self, slower) -> None:
+        self.slower = slower
+
+    def GenerateCube(self, a) ->list[Move]:
+        YOFFSET = 1.15
+        do = []
+        lastMove= Move()
+        # Generowanie dolnej ściany
+        for i in range(0, a * self.slower):
+            for j in range(0, a * self.slower):
+                move = Move((i / self.slower,  YOFFSET, j / self.slower))
+                do.append(move)
+        for i in range(0, a * self.slower):
+            for _ in range(0, a * self.slower):
+                move = Move((lastMove.x + a / self.slower, (i / self.slower)+ YOFFSET, lastMove.z))
+                do.append(move)
+                lastMove = move
+            for _ in range(0, a * self.slower):
+                move = Move((lastMove.x, (i / self.slower)+ YOFFSET, lastMove.z + a / self.slower))
+                do.append(move)
+                lastMove = move
+            for _ in range(0, a * self.slower):
+                move = Move((lastMove.x - a / self.slower,(i / self.slower)+ YOFFSET, lastMove.z))
+                do.append(move)
+                lastMove = move
+            for _ in range(0, a * self.slower):
+                move = Move((lastMove.x, (i / self.slower)+ YOFFSET, lastMove.z - a / self.slower))
+                do.append(move)
+                lastMove = move
+
+        # Generowanie górnej ściany
+        for i in range(0, a * self.slower + 1):
+            for j in range(0, a * self.slower +1):
+                move = Move(((i / self.slower), a+YOFFSET, j / self.slower))
+                do.append(move)
+
+        return do
+    def GenerateSphere(self, radius: float, num_layers: int, num_points_per_layer: int) -> list[Move]:
+        YOFFSET = 1.15
+        do = []
+        for i in range(num_layers):
+            phi = math.pi * (i / (num_layers - 1))  # Ustal phi na podstawie warstwy
+            for j in range(num_points_per_layer):
+                theta = 2 * math.pi * (j / num_points_per_layer)
+                x = radius * math.sin(phi) * math.cos(theta) +0.5
+                y = radius * math.cos(phi) + YOFFSET+1
+                z = radius * math.sin(phi) * math.sin(theta) + 0.5
+                move = Move((x, y, z))
+                do.append(move)
+        do.reverse()
+        return do
 
 app = Ursina()
 
@@ -13,85 +67,39 @@ random.seed(0)
 Entity.default_shader = lit_with_shadows_shader
 
 ground = Entity(model='plane', collider='box', scale=64, texture='grass', texture_scale=(4,4))
+table = Entity(model='plane', collider='box', color=color.gray,position=(0.5,1,0.5),scale=(2,0.1,2))
 
 editor_camera = EditorCamera(enabled=False, ignore_paused=True)
 player = FirstPersonController(model='cube',
-                                z=-10, color=color.orange, origin_y=-.5, speed=8, collider='box')
-player.collider = BoxCollider(player, Vec3(0,1,0), Vec3(1,2,1))
+                                z=-10, color=color.orange, origin_y=-0.5, speed=8, collider='box')
+#player.collider = BoxCollider(player, Vec3(0,1,0), Vec3(1,2,1))
 
-pomel = Entity(model='cube', position=(0,0,0),
-                            scale=(.2,1,.2), origin_z=-.5, color=color.red, on_cooldown=False)
+pomel = Entity(model='cube', position=(0.5, 1.15,0.5),
+                            scale=(.1,.1,.1), origin_z=0, color=color.red, on_cooldown=False)
 
 
 il = 0
-do = []
-lastMove= Move()
-for i in range(0,1*SLOWER):
-    for _ in range (0,1*SLOWER):
-        move = Move((lastMove.x+1/SLOWER,i/10,lastMove.z))
-        do.append(move)
-        lastMove = move
-    for _ in range (0,1*SLOWER):
-        move = Move((lastMove.x,i/10,lastMove.z+1/SLOWER))
-        do.append(move)
-        lastMove = move
-    for _ in range (0,1*SLOWER):
-        move = Move((lastMove.x-1/SLOWER,i/10,lastMove.z))
-        do.append(move)
-        lastMove = move
-    for _ in range (0,1*SLOWER):
-        move = Move((lastMove.x,i/10,lastMove.z-1/SLOWER))
-        do.append(move)
-        lastMove = move
-
+frames = 0
+gen = Generator(SLOWER)
+do = gen.GenerateSphere(1,60,80)
 
 def update():
-    global il
-    if len(do) > il:
-        pomel.position = do[il].get_position()
-        Entity(model='cube', position=(pomel.position), scale=(0.1,0.1,0.1),color=color.white)
-        il+=1
-    pass
-
-
-# class Enemy(Entity):
-#     def __init__(self, **kwargs):
-#         super().__init__(parent=shootables_parent, model='cube', scale_y=2, origin_y=-.5, color=color.light_gray, collider='box', **kwargs)
-#         self.health_bar = Entity(parent=self, y=1.2, model='cube', color=color.red, world_scale=(1.5,.1,.1))
-#         self.max_hp = 100
-#         self.hp = self.max_hp
-
-#     def update(self):
-#         dist = distance_xz(player.position, self.position)
-#         if dist > 40:
-#             return
-
-#         self.health_bar.alpha = max(0, self.health_bar.alpha - time.dt)
-
-
-#         self.look_at_2d(player.position, 'y')
-#         hit_info = raycast(self.world_position + Vec3(0,1,0), self.forward, 30, ignore=(self,))
-#         # print(hit_info.entity)
-#         if hit_info.entity == player:
-#             if dist > 2:
-#                 self.position += self.forward * time.dt * 5
-
-#     @property
-#     def hp(self):
-#         return self._hp
-
-#     @hp.setter
-#     def hp(self, value):
-#         self._hp = value
-#         if value <= 0:
-#             destroy(self)
-#             return
-
-#         self.health_bar.world_scale_x = self.hp / self.max_hp * 1.5
-#         self.health_bar.alpha = 1
-
-# Enemy()
-#enemies = [Enemy(x=x*4) for x in range(4)]
+    global frames
+    if frames > 1:
+        global il
+        if len(do) > il:
+            pomel.set_position(do[il].get_position())
+            if not do[il].only_move:
+                pos = pomel.position
+                pos[0] += 0
+                pos[1] -= 0.1
+                pos[2] += 0
+                Entity(model='cube', position=(pos), scale=(1/SLOWER,1/SLOWER,1/SLOWER),color=color.white)
+            il+=1
+        else:
+            pomel.disable()
+        frames = 0
+    frames +=1
 
 
 def pause_input(key):
@@ -113,5 +121,4 @@ pause_handler = Entity(ignore_paused=True, input=pause_input)
 sun = DirectionalLight()
 sun.look_at(Vec3(1,-1,-1))
 Sky()
-
 app.run()
