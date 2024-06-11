7704fd1c15c6601f626dc6b3c585a5152bc4e0a8
Mateusz Krajewski <matikrajek42@gmail.com>
2024-06-11 14:33:22 +0200

    force fixx

--- Generator.py
+++ Generator.py
@@ -32,6 +32,9 @@ class Generator():
         do = []
         lastMove= Move()
         # Generowanie dolnej ściany
+        do = []
+        lastMove= Move()
+        # Generowanie dolnej ściany
         for i in range(0, a * self.slower):
             for j in range(0, a * self.slower):
                 move = Move((i / self.slower,  self.YOFFSET, j / self.slower))
@@ -39,23 +42,23 @@ class Generator():
 
         for i in range(0, a * self.slower):
             for _ in range(0, a * self.slower):
-                move = Move((lastMove.x + self.XZOFFSET / self.slower,
+                move = Move((lastMove.x + self.XZOFFSET / (2*self.slower),
                                     (i / self.slower)+ self.YOFFSET, lastMove.z))
                 do.append(move)
                 lastMove = move
             for _ in range(0, a * self.slower):
                 move = Move((lastMove.x, (i / self.slower)+ self.YOFFSET,
-                                            lastMove.z + self.XZOFFSET / self.slower))
+                                            lastMove.z + self.XZOFFSET / (self.slower*2)))
                 do.append(move)
                 lastMove = move
             for _ in range(0, a * self.slower):
-                move = Move((lastMove.x - self.XZOFFSET / self.slower,
+                move = Move((lastMove.x - self.XZOFFSET / (2*self.slower),
                                             (i / self.slower)+ self.YOFFSET, lastMove.z))
                 do.append(move)
                 lastMove = move
             for _ in range(0, a * self.slower):
                 move = Move((lastMove.x, (i / self.slower)+ self.YOFFSET,
-                                                lastMove.z - self.XZOFFSET / self.slower))
+                                                lastMove.z - self.XZOFFSET / (2*self.slower)))
                 do.append(move)
                 lastMove = move
 
